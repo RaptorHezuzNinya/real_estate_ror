cbb0d951a4a4c50b035346c76996647034825214
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-28 18:19:29 +0100

    spec folder

---
+++ spec/models/tenant_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Tenant, type: :model do
+  it { should have_many(:payments).dependent(:destroy)}
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:accountholder)}
+  it { should validate_presence_of(:first_name)}
+  it { should validate_presence_of(:last_name)}
+  it { should validate_presence_of(:iban)}
+  it { should validate_presence_of(:rent)}
+  it { should validate_presence_of(:phone)}
+end
