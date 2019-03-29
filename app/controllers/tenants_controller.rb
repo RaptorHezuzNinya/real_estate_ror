a102f05aa626ea53193655eb0f18507f6437b1e8
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-29 14:50:43 +0100

    first route works

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,2 +1,8 @@
 class TenantsController < ApplicationController
+    before_action :set_tenant, only: [:show]
+
+    def index
+        @tenants = Tenant.all
+        render json: @tenants
+    end
 end
