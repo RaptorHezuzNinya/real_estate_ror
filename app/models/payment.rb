10f35ba8663955a67cb122391972372d88312ee4
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-04-07 12:59:01 +0200

    add validations and rack cors

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
   belongs_to :tenant
 
-  validates_presence_of :iban, :amount, :payment_json, :date
+  validates_presence_of :iban, :amount, :payment_json, :date, :account_holder
 end
