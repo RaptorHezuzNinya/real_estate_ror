a0ac2fd928dd2cd78376ab138a9826cf00055143
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-04-07 12:59:19 +0200

    new factory for opaymebnt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_164917) do
+ActiveRecord::Schema.define(version: 2019_04_03_224300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,5 +39,21 @@ ActiveRecord::Schema.define(version: 2019_03_28_164917) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "uploads", force: :cascade do |t|
+    t.json "data"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_uploads_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "payments", "tenants"
+  add_foreign_key "uploads", "users"
 end
