cffb33efa5218b719471bac794ab127481232237
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-04-07 12:59:10 +0200

    update schema with validations

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,5 @@
 class Tenant < ApplicationRecord
     has_many :payments, dependent: :destroy
 
-    validates_presence_of :email, :accountholder, :first_name, :last_name, :iban, :rent, :phone
+    validates_presence_of :email, :account_holder, :first_name, :last_name, :iban, :rent, :phone
 end
